4c5c89d12e0c878018e0b6184be298fba9193714
Mesquita <mesquitta@gmail.com>
2016-10-25 20:27:04 -0200

    Nova alteração

--- src/br/com/caelum/jdbc/TestaListagem.java
+++ src/br/com/caelum/jdbc/TestaListagem.java
@@ -12,22 +12,22 @@ public class TestaListagem {
 	public static void main(String[] args) throws SQLException {
 		
 		Connection connection = DriverManager.getConnection(
-				"jdbc:mysql://localhost/cwd_eai_kibon?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&autoReconnect=true&useSSL=false",
+				"jdbc:mysql://localhost/loja_virtual?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&autoReconnect=true&useSSL=false",
 				"root", "nielsenmanon");
 		
 		Statement statement = connection.createStatement();
-		boolean resultado = statement.execute("select * from cwreturntemplatemessages limit 15;");
+		boolean resultado = statement.execute("select * from produto");
 		ResultSet resultSet = statement.getResultSet();
 
 		while (resultSet.next()) {
-			long id = resultSet.getLong("id");
+			int id = resultSet.getInt("id");
 			System.out.println("Id: " + id);
 
-			String VehicleNumber = resultSet.getString("VehicleNumber");
-			System.out.println("Número do veículo: " + VehicleNumber);
+			String nome = resultSet.getString("nome");
+			System.out.println("Número do veículo: " + nome);
 
-			String Message = resultSet.getString("Message");
-			System.out.println("Messagem: " + Message);
+			String descricao = resultSet.getString("descricao");
+			System.out.println("Messagem: " + descricao);
 		}
 		
 		resultSet.close();
