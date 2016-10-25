bdedcfbea8e9179cfd203225c44f21c68590c1b4
Mesquita <mesquitta@gmail.com>
2016-10-25 20:29:08 -0200

    troca do banco

--- src/br/com/caelum/jdbc/TestaConexao.java
+++ src/br/com/caelum/jdbc/TestaConexao.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class TestaConexao {
 	public static void main(String[] args) throws SQLException{
 		Connection connection = 
-				DriverManager.getConnection("jdbc:mysql://localhost/cwd_eai_kibon?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&autoReconnect=true&useSSL=false", "root", "nielsenmanon");
+				DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&autoReconnect=true&useSSL=false", "root", "nielsenmanon");
 		
 		System.out.println("abrindo uma conexao com o banco de dados");
 		
